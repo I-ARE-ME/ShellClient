f0d477c671004d19f7feb641d8199b3ca267f377
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-09-11 17:12:17 -0400

    ⚡️ Created script

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         ⚡️ShellClient
+// @namespace    https://sites.google.com/view/foxdev
+// @version      1.2
+// @description  A client for shell shockers
+// @run-at       document-body
+// @author       ItsFoxDev
+// @match        https://shellshock.io/*
+// @icon         COMING SOON
+// @grant        none
+// ==/UserScript==
